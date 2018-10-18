430af087216b80b6a18191e777b8b663094059a2
Aaron <aaron.stbn@gmail.com>
2018-10-18 22:28:42 +0000

    migration test

--- migrations/20181018211038-create_user_table.js
+++ migrations/20181018211038-create_user_table.js
@@ -3,7 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', { 
-      id         : Sequelize.INTEGER,
+      id         : {
+        type         : Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey   : true
+      },
       email      : Sequelize.STRING,
       workplaceId: Sequelize.STRING
     });

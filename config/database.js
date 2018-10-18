7dbc7375257abf8e4d445a93a8422ba32f274715
Aaron <aaron.stbn@gmail.com>
2018-10-18 21:57:29 +0000

    test

--- config/database.js
+++ config/database.js
@@ -2,6 +2,7 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOSTNAME,
-    dialect: 'postgres'
+    host    : process.env.DB_HOSTNAME,
+    dialect : 'postgres',
+    ssl     : true
 };

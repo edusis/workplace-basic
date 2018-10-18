dfe6b1328dae8f50b68371364520802afd249831
Aaron <aaron.stbn@gmail.com>
2018-10-18 22:59:00 +0000

    format issue

--- controllers/jira.js
+++ controllers/jira.js
@@ -17,12 +17,33 @@ router.post("/", function(req, res) {
   let issueCode  = data.issue.key;
   let summary    = data.issue.fields.summary;
   let issueUrl   = data.issue.self;
+  let reporter   = data.issue.reporter;
+  let assignee   = data.issue.assignee;
   
+  let messageText = `* Codigo: ${issueCode}`
+  
+  messageText +=`\n* Proyecto: BMDL`
+  
+  console.log(reporter);
+  console.log(assignee);
+  if(reporter){
+    messageText+=`\n* Solicitante: Claudio Solis`  
+  }
+  
+  if(assignee){
+    messageText+=`\n* AgileOps: Miguel Canchica`  
+  }
+  
+  if(summary){
+    messageText+=`\n* ${summary}`
+  }
+
+  let testId = "100029033602415"
   
   //TODO: AQUI SE DEBE HACER MATCH DEL CORREO DEL USUARIO CON UN ID GUARDADO EN LA BASE DE DATOS Y ENVIARLE UN MENSAJE
   //userService.getUserByEmail()..;
   
-  //facebookGraphService.sendTextMessage(CHAT_ID_USER, CHAT_MESSAGE);
+  facebookGraphService.sendTextMessage(testId, messageText);
   
   console.log(fromStatus,toStatus,issueId,issueCode,summary);
   return res.sendStatus(200);

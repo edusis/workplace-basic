b59ba73c152c3ff8ed96ff1b7a6ff0bcc9da3fe5
Aaron <aaron.stbn@gmail.com>
2018-10-19 21:04:02 +0000

    tst fix

--- testjira.js
+++ testjira.js
@@ -1,6 +1,6 @@
 const jiraService = require("./services/jira");
 
-let issueCode = "ACR-39";
+let issueCode = "11267";
 let transitionId = "3";
 let comment = "Se aprobo por seguridad mediante Release Bot";
 

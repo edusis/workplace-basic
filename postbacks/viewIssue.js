bebfde2e814fc8bbfae41edf74ec7d47819c1f48
Aaron <aaron.stbn@gmail.com>
2018-10-22 20:08:53 +0000

    fix

--- postbacks/viewIssue.js
+++ postbacks/viewIssue.js
@@ -4,7 +4,7 @@ module.exports = function(event){
     let senderID      = event.sender.id;
     let recipientID   = event.recipient.id;
     let timeOfMessage = event.timestamp;
-    let payload       = event.payload;
+    let payload       = event.postback.payload;
     
     let payloadParsed = JSON.parse(payload);
     let issueCodeV    = payloadParsed.params["issueCode"];

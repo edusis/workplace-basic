13469cda59e80e582aa79b182d85ac9432300144
Aaron <aaron.stbn@gmail.com>
2018-10-23 16:40:05 +0000

    fix

--- postbacks/viewIssue.js
+++ postbacks/viewIssue.js
@@ -4,12 +4,9 @@ const jiraService          = require("../services/jira");
 
 module.exports = function(event){
     let senderID      = event.sender.id;
-    let recipientID   = event.recipient.id;
-    let timeOfMessage = event.timestamp;
     let payload       = event.postback.payload;
     
     let payloadParsed = JSON.parse(payload);
-    let issueCode     = payloadParsed.params["issueCode"];
     let issueId       = payloadParsed.params["issueId"];
     
     asyncLib.waterfall([
@@ -17,7 +14,7 @@ module.exports = function(event){
             return jiraService.getIssueById(issueId,next)
         },
         function(rawIssue,next){
-            return facebookGraphService.sendIssueQuickReply(recipientID,rawIssue,next)            
+            return facebookGraphService.sendIssueQuickReply(senderID,rawIssue,next)            
         }
     ],
     function(err,response){
